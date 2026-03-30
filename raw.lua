a1e2feafd552f6ab38dcdf1bec670c040ced9054
arlynfrance <arlynfranceny@gmail.com>
2026-03-30 05:03:03 -0700

    Update raw.lua

--- raw.lua
+++ raw.lua
@@ -69,7 +69,7 @@ UserInputService.InputBegan:Connect(function(input, gameProcessed)
     if input.KeyCode == Enum.KeyCode.Q then
         -- Example usage: extend hitbox by 10 studs for 1 second
         -- Adjust 'studs' and 'duration' as needed
-        lolz:ExtendHitbox(11.67, 0.7)
+        lolz:ExtendHitbox(11.50, 0.6)
     end
 end)
 
